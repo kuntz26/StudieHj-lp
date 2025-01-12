81d4c7343f00ac5b85d483dc90dfbb95234d76fd
AndMore-bit <felixjensen5555@gmail.com>
2025-01-12 11:21:04 +0100

    Budget og login og ud funktioner virker

---
+++ static/Tables.sql
@@ -0,0 +1,55 @@
+CREATE TABLE IF NOT EXISTS people (
+    id              INTEGER NOT NULL,
+    --name            TEXT,
+    username        TEXT NOT NULL UNIQUE,
+    filename        TEXT,
+    PRIMARY KEY(id AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS login (
+    personid    INTEGER NOT NULL,
+    email       TEXT NOT NULL,
+    password    TEXT NOT NULL,
+    FOREIGN KEY(personid) REFERENCES people(id)
+);
+
+CREATE TABLE IF NOT EXISTS privateMessages (
+    id          INTEGER NOT NULL,
+    senderid    INTEGER NOT NULL,
+    recipientid INTEGER NOT NULL,
+    message     TEXT NOT NULL,
+    date        DATE NOT NULL,
+    PRIMARY KEY(id AUTOINCREMENT),
+    FOREIGN KEY(senderid) REFERENCES people(id),
+    FOREIGN KEY(recipientid) REFERENCES people(id)
+);
+
+CREATE TABLE IF NOT EXISTS publicMessages (
+    id          INTEGER NOT NULL,
+    senderid    INTEGER NOT NULL,
+    header      TEXT NOT NULL,
+    message     TEXT NOT NULL,
+    categoryid  INTEGER NOT NULL,
+    date        DATE NOT NULL,
+    -- Add pictures or files
+    PRIMARY KEY(id AUTOINCREMENT),
+    FOREIGN KEY(senderid) REFERENCES people(id),
+    FOREIGN KEY(categoryid) REFERENCES category(id)
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+    id          INTEGER NOT NULL,
+    date        DATE NOT NULL,
+    messageid   INEGER NOT NULL,
+    comment     TEXT NOT NULL,
+    senderid    INTEGER NOT NULL,
+    PRIMARY KEY(id AUTOINCREMENT),
+    FOREIGN KEY(senderid) REFERENCES people(id),
+    FOREIGN KEY(messageid) REFERENCES publicMessages(id)
+);
+
+CREATE TABLE IF NOT EXISTS category (
+    id          INTEGER NOT NULL,
+    name        TEXT NOT NULL,
+    PRIMARY KEY(id AUTOINCREMENT)
+);
