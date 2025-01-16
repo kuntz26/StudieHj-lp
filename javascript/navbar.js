afe2119fc9b2b08936670d0b8fb7ae2370991b9a
kuntz26 <149362652+kuntz26@users.noreply.github.com>
2025-01-16 09:29:18 +0100

    tilføj logo

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,3 +1,10 @@
+// Forhindrer lukning af dropdown-menu, når der klikkes inde i den
 $(document).on("click", ".navbar-right .dropdown-menu", function (e) {
 	e.stopPropagation();
+});
+
+// Tilføj interaktion med logoet (f.eks. klik på logoet fører til en specifik side)
+$(document).on("click", ".navbar-brand .logo", function () {
+	alert("Du klikkede på logoet!"); // Tilføj en handling her, f.eks. redirigering
+	// window.location.href = "index.html"; // Eksempel: Naviger til hjemmesidens forside
 });
